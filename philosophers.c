5e748aabc9f5a9e803bced92a77b086a54100827
Filipe Andre Pinho De Oliveira <fpinho-d@c1r14s14.42porto.com>
2023-08-14 19:26:09 +0100

    philos 14 agosto 42 v1

---
+++ philosophers.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpinho-d <fpinho-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 18:49:23 by fpinho-d          #+#    #+#             */
+/*   Updated: 2023/08/14 19:01:12 by fpinho-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+// gcc -g -pthread philosophers.c -o philosophers
+
+void *routine()
+{
+	printf("routine created\n");
+	sleep(3);
+	printf("end routine\n");
+}
+
+
+int	main(int ac, char **av)
+{
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create (&t1, NULL, &routine, NULL);
+	pthread_create (&t2, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	return (0);
+}
---
+++ philosophers.h
@@ -0,0 +1,10 @@
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+
+#endif
